f20716e464a746278f35cfc2a0fbe450f061c06a
AvhikBiswas <abhikbiswas351@gmail.com>
2026-07-22 19:17:37 +0530

    resume added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/resume',
+        destination: 'https://shorturl.at/yUCLq',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
